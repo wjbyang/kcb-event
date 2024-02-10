6885be04a348abfaa4a3238db1bfa9bb9df59958
Mark Bu <peashooterking8@gmail.com>
2024-02-10 14:26:01 -0500

    added urls for event delete/update

--- eventScheduler/urls.py
+++ eventScheduler/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('v1/user/<str:user_id>', views.ViewUser.as_view(), name='user_get_one'),
     path('v1/users', views.ViewUsers.as_view(), name='user_get_all'),
     path('v1/user/update/<str:user_id>', views.UpdateUser.as_view(), name='user_update'),
-    path('v1/user/delete/<str:user_id>', views.ViewUser.as_view(), name='user_delete'),
+    path('v1/user/delete/<str:user_id>', views.DeleteUser.as_view(), name='user_delete'),
     path('v1/organization', views.OrganizationView.as_view(), name='organization_post'),
     path('v1/organization/<str:organization_id>', views.ViewOrganization.as_view(), name='organization_get_one'),
     path('v1/organizations', views.ViewOrganizations.as_view(), name='organization_get_all'),
@@ -15,4 +15,6 @@ urlpatterns = [
     path('v1/event', views.EventView.as_view(), name='event_post'),
     path('v1/event/<str:event_id>', views.ViewEvent.as_view(), name='event_get_one'),
     path('v1/events', views.ViewEvents.as_view(), name='event_get_all'),
+    path('v1/event/update/<str:event_id>', views.UpdateEvent.as_view(), name='event_update'),
+    path('v1/event/delete/<str:event_id>', views.DeleteEvent.as_view(), name='event_delete'),
 ]
